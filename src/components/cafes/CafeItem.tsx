274105cc5ef12b7e06b7264392abef3f193b945e
hanyoung <phanyoung@naver.com>
2025-01-04 17:19:45 +0900

    feat(CafeItem.tsx): CafeItem 컴포넌트 생성

---
+++ src/components/cafes/CafeItem.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { cafeImageList, cafeItemHeading, cafeItemName, tagList, temporaryTag } from './cafes.css';
+import { ROUTE_PATH } from '@/constants/routePath';
+
+interface Cafe {
+  id: number;
+  name: string;
+  location: string;
+  tags: { id: number; name: string }[];
+  images: string[];
+}
+
+interface CafeItemProps {
+  cafe: Cafe;
+}
+
+export default function CafeItem({ cafe }: CafeItemProps) {
+  const { id, name, location, tags, images } = cafe;
+
+  return (
+    <Link href={`${ROUTE_PATH.cafe}/${id}`}>
+      <div>
+        <div className={cafeItemHeading}>
+          <div className={cafeItemName}>{name}</div>
+          <div>{location}</div>
+        </div>
+        <div className={tagList}>
+          {tags.map((tag, index) => (
+            <TemporaryTag key={`${name}-tag-${index}`} name={tag.name} />
+          ))}
+        </div>
+        <div className={cafeImageList}>
+          {images.map((src, index) => (
+            <Image
+              key={`${name}-image-${index}`}
+              alt={`${name} ${index + 1}번째 사진`}
+              src={src}
+              height="200"
+              width="200"
+            />
+          ))}
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+function TemporaryTag({ name }: { name: string }) {
+  return <div className={temporaryTag}>{name}</div>;
+}
