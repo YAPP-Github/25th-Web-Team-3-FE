54699d2d44229fd99337b7487caff820397cadc4
hanyoung <phanyoung@naver.com>
2025-01-14 17:47:35 +0900

    refactor: FilterButtons -> RegionSelectButtons로 이름 수정

--- src/app/cafes/page.tsx
+++ src/app/cafes/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
 import { useState } from 'react';
-import RegionSelectModal from '@/components/cafes/RegionSelectModal';
 import CafeList from '@/components/cafes/CafeList';
 import MOCK_CAFE_LIST from '@/mock/cafeList.json';
 import type { Region } from '@/types';
+import RegionSelectModal from '@/components/cafes/RegionSelectModal';
+import RegionSelectButtons from '@/components/cafes/RegionSelectButtons';
 import { REGIONS } from '@/constants/region';
-import FilterButtons from '@/components/cafes/FilterButtons';
 
 export default function Page() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [region, setRegion] = useState<Region>(REGIONS.전체);
 
   const resetRegion = () => {
-    setRegion('전체');
+    setRegion(REGIONS.전체);
   };
 
   const openModal = () => {
@@ -26,10 +26,14 @@ export default function Page() {
 
   return (
     <div>
-      <div>현재 선택된 지역: {region}</div>
-      <FilterButtons region={region} resetRegion={resetRegion} openModal={openModal} />
+      <RegionSelectButtons region={region} resetRegion={resetRegion} openModal={openModal} />
       <CafeList cafeList={MOCK_CAFE_LIST} />
-      <RegionSelectModal isOpen={isModalOpen} setRegion={setRegion} onClose={closeModal} />
+      <RegionSelectModal
+        isOpen={isModalOpen}
+        region={region}
+        setRegion={setRegion}
+        onClose={closeModal}
+      />
     </div>
   );
 }
