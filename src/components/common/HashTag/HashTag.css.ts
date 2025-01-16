3f1fa6b1a0d9c70978e91104ed23b50bda147c22
황유빈 <bnm0218@naver.com>
2025-01-17 04:00:50 +0900

    feat(Hashtag):공통 Hashtag 구현

--- src/components/common/HashTag/HashTag.css.ts
+++ src/components/common/HashTag/HashTag.css.ts
@@ -1,13 +1,27 @@
+import { color } from '@/styles/color.css';
+import { caption } from '@/styles/typo.css';
 import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 export const hashTag = style({
+  ...caption,
   display: 'flex',
-  gap: '0.6rem',
 });
 
-export const hashTagItem = style({
-  backgroundColor: '#F7F7F7',
-  padding: '0.3rem 0.6rem',
-  borderRadius:"0.4rem",
-  opacity:'50%'
+export const hashTagItem = recipe({
+  variants: {
+    isCafesPage: {
+      true: {
+       flex:'1',
+       textAlign:'center'
+
+      },
+      false: {
+        backgroundColor: '#F7F7F7',
+        padding: '0.3rem 0.6rem',
+        borderRadius: '0.4rem',
+        opacity: '50%',
+      },
+    },
+  },
 });
--- src/components/common/HashTag/index.tsx
+++ src/components/common/HashTag/index.tsx
@@ -1,13 +1,58 @@
+'use client';
+import { usePathname } from 'next/navigation';
 import { hashTag, hashTagItem } from './HashTag.css';
+import baristar from '@/assets/Icon/baristar.svg';
+import roastery from '@/assets/Icon/roastery.svg';
+import dirCoffee from '@/assets/Icon/dripCoffee.svg';
+import signiture from '@/assets/Icon/signiture.svg';
+import Image from 'next/image';
 
-const HASH_TAG_DATA = ['전문 바리스타', '로스터리', '드립커피', '시그니처'];
+const HASH_TAG_DATA = [
+  {
+    image: baristar,
+    name: '전문 바리스타',
+  },
+  {
+    image: roastery,
+    name: '로스터리',
+  },
+  {
+    image: dirCoffee,
+    name: '드립커피',
+  },
+  {
+    image: signiture,
+    name: '시그니처',
+  },
+];
 
 export function HashTag() {
+  const pathName = usePathname();
+
+  // 상세 페이지 여부를 판단
+  function isDetailPage(pathName?: string): boolean {
+    if (!pathName) return false;
+    const segments = pathName.split('/');
+    return segments[segments.length - 2] === 'cafes' && !!segments[segments.length - 1];
+  }
+
+  // cafes 페이지 여부 확인
+  const isCafesPage = isDetailPage(pathName);
+
   return (
-    <div className={hashTag}>
-      {HASH_TAG_DATA.map((item) => (
-        <div className={hashTagItem}>{item}</div>
+    <ul className={hashTag}>
+      {HASH_TAG_DATA.map((tag, index) => (
+        <li key={index} className={hashTagItem({ isCafesPage })}>
+          {isCafesPage ? (
+            <div>
+              <Image src={tag.image} alt={tag.name} />
+              <div>{tag.name}</div>
+            </div>
+          ) : (
+            <span>{tag.name}</span>
+          )}
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
