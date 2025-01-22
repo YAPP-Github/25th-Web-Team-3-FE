082ea52bdd9a1b00f2a6498744ab33b89470a24c
황유빈 <bnm0218@naver.com>
2025-01-23 02:10:24 +0900

    design:상세페이지 UI 피드백 수정사항 반영

--- src/components/cafes/[id]/RoastingBar/RoastingBar.css.ts
+++ src/components/cafes/[id]/RoastingBar/RoastingBar.css.ts
@@ -1,6 +1,6 @@
 import { color } from '@/styles/color.css';
 import { body1 } from '@/styles/typo.css';
-import { style } from '@vanilla-extract/css';
+import { ComplexStyleRule, globalStyle, style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
 export const roastingBar = style({
@@ -16,11 +16,6 @@ export const roastingSegment = recipe({
     height: '100%',
     position: 'relative',
     color: color.grayScale.gray400,
-    selectors: {
-      '&:not(:last-child)': {
-        borderRight: `1px solid ${color.grayScale.gray500}`,
-      },
-    },
   },
   variants: {
     isActive: {
@@ -63,17 +58,24 @@ export const roastingStatus = recipe({
     },
   },
 });
-
-export const roastingSpan = style({
-  selectors: {
-    '&:nth-child(1)': {
-      color: color.grayScale.gray500,
-    },
-    '&:nth-child(2)': {
-      color: color.grayScale.gray500,
-    },
-    '&:nth-child(3)': {
-      color: color.grayScale.gray500,
+export const roastingText = recipe({
+  base: {
+    color: color.grayScale.gray400,
+    flex: '1',
+  },
+  variants: {
+    isActive: {
+      true: {
+        color: color.grayScale.gray500,
+      },
     },
   },
+});
+
+globalStyle(`${roastingStatus()} span:nth-child(2)`, {
+  textAlign: 'center'
+});
+
+globalStyle(`${roastingStatus()} span:nth-child(3)`, {
+  textAlign: 'right'
 });
--- src/components/cafes/[id]/RoastingBar/index.tsx
+++ src/components/cafes/[id]/RoastingBar/index.tsx
@@ -1,28 +1,33 @@
-import { roastingBar, roastingSegment, roastingStatus } from './RoastingBar.css';
+import { roastingBar, roastingSegment, roastingStatus, roastingText } from './RoastingBar.css';
 
-export const RoastingBar = ({ activeLevel }: { activeLevel: 'light' | 'medium' | 'dark' }) => (
-  <>
-    <div className={roastingBar}>
-      <div
-        className={roastingSegment({
-          isActive: activeLevel === 'light' ? 'light' : undefined,
-        })}
-      ></div>
-      <div
-        className={roastingSegment({
-          isActive: activeLevel === 'medium' ? 'medium' : undefined,
-        })}
-      ></div>
-      <div
-        className={roastingSegment({
-          isActive: activeLevel === 'dark' ? 'dark' : undefined,
-        })}
-      ></div>
-    </div>
-    <div className={roastingStatus({ isActive: activeLevel })}>
-      <span>라이트</span>
-      <span>미디움</span>
-      <span>다크</span>
-    </div>
-  </>
-);
+type RoastingLevelType = 'light' | 'medium' | 'dark' | undefined;
+
+export const RoastingBar = ({ activeLevel }: { activeLevel: 'light' | 'medium' | 'dark' }) => {
+  const roastingLevel = activeLevel.toLowerCase() as RoastingLevelType;
+  return (
+    <>
+      <div className={roastingBar}>
+        <div
+          className={roastingSegment({
+            isActive: roastingLevel === 'light' ? 'light' : undefined,
+          })}
+        ></div>
+        <div
+          className={roastingSegment({
+            isActive: roastingLevel === 'medium' ? 'medium' : undefined,
+          })}
+        ></div>
+        <div
+          className={roastingSegment({
+            isActive: roastingLevel === 'dark' ? 'dark' : undefined,
+          })}
+        ></div>
+      </div>
+      <div className={roastingStatus({ isActive: roastingLevel })}>
+        <span className={roastingText({ isActive: roastingLevel === 'light' })}>라이트</span>
+        <span className={roastingText({ isActive: roastingLevel === 'medium' })}>미디움</span>
+        <span className={roastingText({ isActive: roastingLevel === 'dark' })}>다크</span>
+      </div>
+    </>
+  );
+};
