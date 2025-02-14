7e33e432bed806b8057397732fae64a741990959
황유빈 <bnm0218@naver.com>
2025-02-15 02:56:49 +0900

    feat(bookmark): 북마크 폴더 내의 저장된 북마크 삭제

--- src/components/bookmarks/[id]/SavedBookmarksList/index.tsx
+++ src/components/bookmarks/[id]/SavedBookmarksList/index.tsx
@@ -1,101 +1,65 @@
 import PopUpButton from '@/components/common/PopUpButton';
-import Image from 'next/image';
-import CheckBox from '../CheckBox';
-import {
-  buttonSection,
-  listContainer,
-  listItem,
-  listItemDivider,
-  listItemLocation,
-  listItemName,
-  listItemTextBox,
-  nonListButton,
-  nonListContents,
-  nonListText,
-  nonListTitle
-} from './SavedBookmark.css';
-import { useRouter } from 'next/navigation';
-import { ROUTE_PATH } from '@/constants/routePath';
+import EmptyListMessage from './EmptyListMessage';
+import { buttonSection, listContainer } from './SavedBookmark.css';
+import SavedBookmarkItem from './SavedBookmarkItem';
+import { Cafe } from '../../BookmarkList/hooks/useBookmarkList';
 
+
+interface SelectedBookmarkList {
+  id: string;
+  listName: string;
+  cafes?: Cafe[]
+}
 interface SavedBookmarkListProps {
-  detailFrom: {
-    id: string;
-    name: string;
-    location: string;
-    mainImageUrl: string;
-  }[];
+  listId: string;
+  selectedBookmarkList: SelectedBookmarkList[];
   isEdit: boolean;
-  setIsEdit: React.Dispatch<React.SetStateAction<boolean>>;
   selectedIds: string[];
-  setSelectedIds: React.Dispatch<React.SetStateAction<string[]>>;
+  onCancelEdit: () => void;
   onDelete: (ids: string[]) => void;
+  onSelectItem: (id: string) => void;
 }
 
 export default function SavedBookmarkList({
-  detailFrom,
+  listId,
+  selectedBookmarkList,
   isEdit,
-  setIsEdit,
   selectedIds,
-  setSelectedIds,
+  onCancelEdit,
+  onSelectItem,
   onDelete,
 }: SavedBookmarkListProps) {
-  const handleCheck = (id: string) => {
-    setSelectedIds((prev) =>
-      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
-    );
+  const folder = selectedBookmarkList.find((folder) => folder.id === listId);
+  const cafes = folder?.cafes ?? [];
+
+  const handleItemCheck = (id: string) => {
+    onSelectItem(id);
   };
-  const router=useRouter ()
+
+  if (cafes.length === 0) {
+    return <EmptyListMessage />;
+  }
 
   return (
     <ul className={listContainer}>
-      {detailFrom.length > 0 ? (
-        detailFrom.map((bookmark) => (
-          <div key={bookmark.id}>
-            <li className={listItem}>
-              {isEdit && (
-                <CheckBox
-                  isEdit={isEdit}
-                  checked={selectedIds.includes(bookmark.id)} 
-                  onCheck={() => handleCheck(bookmark.id)} 
-                />
-              )}
-              <Image
-                src={`${bookmark.cafes.mainImageUrl}`}
-                alt={`${bookmark.cafes.name}`}
-                width={75}
-                height={75}
-              />
-              <div className={listItemTextBox}>
-                <h2 className={listItemName}>{bookmark.name}</h2>
-                <p className={listItemLocation}>{bookmark.location}</p>
-              </div>
-            </li>
-            <div className={listItemDivider}></div>
-          </div>
-        ))
-      ) : (
-        <div className={nonListContents}>
-          <h2 className={nonListTitle}>저장된 카페가 없어요</h2>
-          <p className={nonListText}>마음에 드는 카페를 지정해보세요</p>
-          <button className={nonListButton} onClick={()=>router.push(ROUTE_PATH.cafes)}>카페 둘러보기</button>
-        </div>
-      )}
+      {[...cafes].reverse().map((bookmark) => (
+        <SavedBookmarkItem
+          key={bookmark.id}
+          cafe={bookmark}
+          isEdit={isEdit}
+          selectedIds={selectedIds}
+          onItemCheck={handleItemCheck}
+          listId={listId}
+        />
+      ))}
       {isEdit && (
         <section className={buttonSection}>
-          <PopUpButton
-            title="취소"
-            onClick={() => {
-              setIsEdit(false);
-              setSelectedIds([]);
-            }}
-          />
+          <PopUpButton title="취소" onClick={onCancelEdit} />
           <PopUpButton
             title="선택 삭제"
             color="black"
             onClick={() => {
               onDelete(selectedIds);
-              setSelectedIds([]);
-              setIsEdit(false);
             }}
           />
         </section>
