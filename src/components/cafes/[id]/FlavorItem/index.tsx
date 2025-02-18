e046fcbc747c9e7e088123624a6b4c94a0dd4641
황유빈 <bnm0218@naver.com>
2025-02-18 17:40:18 +0900

    fix(FlavorItem):category 응답 값 수정으로 인한 예외 처리 삭제

--- src/components/cafes/[id]/FlavorItem/index.tsx
+++ src/components/cafes/[id]/FlavorItem/index.tsx
@@ -23,10 +23,7 @@ export default function FlavorList({ flavors }: FlavorListProps) {
   return (
     <ul className={flavorList}>
       {flavors.map((flavor) => {
-        //api 응답 값 오타 수정 후 코드 수정 예정입니다!
-        const category = flavor.category
-          ? (flavor.category.toLowerCase() as FlavorCategory)
-          : ('other' as FlavorCategory);
+        const category = flavor.category.toLowerCase() as FlavorCategory;
         return (
           <HashTag key={`${flavor.name}-${flavor.category}`}>
             <li className={flavorItem}>
